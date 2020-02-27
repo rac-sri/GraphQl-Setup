9838130519a5a478b8f2102b7e087b6e66ea72d3
rachit2501 <rac.sri25@gmail.com>
2020-02-27 18:25:10 +0530

    bugs in demo

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,24 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_ , __ , ctx){
+      return {id:3}
     }
   },
-  User: {
+ // Mutation: {
     
-  }
+//},
+  Pet: {
+    id(){
+      return 3
+    }
+    // img(pet) {
+    //   return pet.type === 'DOG'
+    //     ? 'https://placedog.net/300/300'
+    //     : 'http://placekitten.com/300/300'
+    // }
+  },
+  // User: {
+    
+  // }
 }
--- demo.js
+++ demo.js
@@ -4,14 +4,29 @@ const typeDefs = gql`
 type User {
     email: String!
     avatar: String
-    friends: [User]!
+    friends: [User] 
 }
  type Query {
      me: User!
+     shoes(input:ShoesInput): [Shoe]!
+ }
+
+ type Shoe {
+     brand: String!
+     size: Int!
+ }
+
+ input ShoesInput{
+     brand:String
+     size: Int
  }
 `
 const resolver = {
     Query:{
+        shoes(_, {input}){
+            return [{brand:'nike', size:12},
+                    {brand:'puma', size:10}]
+        },
         me(){
             return {
                 email: 'rac.sru3@gmail.com',
